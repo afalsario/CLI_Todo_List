fdfeb64e00c6efdb93f031707c103d970fafc044
Ashley <a_falsario@yahoo.com>
2014-05-29 10:14:30 -0500

    added save option

--- todo.php
+++ todo.php
@@ -9,17 +9,13 @@ function list_items($list)
     $result = '';
     foreach ($list as $key => $value)
     {
+//Return string of list items separated by newlines.
         $result .= "[" . ($key + 1) . "] TODO $value" . PHP_EOL;
     }
     return $result;
-
-    //Return string of list items separated by newlines.
-    // Should be listed [KEY] Value like this:
-    // [1] TODO item 1
-    // [2] TODO item 2 - blah
-    // DO NOT USE ECHO, USE RETURN
 }
 
+//****************************************************************************
 // Get STDIN, strip whitespace and newlines,
 // and convert to uppercase if $upper is true
 function get_input($upper = false)
@@ -28,6 +24,7 @@ function get_input($upper = false)
     return $upper ? strtoupper($result) : $result;
 }
 
+//****************************************************************************
 //Sort menu
 function sort_menu($items)
 {
@@ -50,34 +47,48 @@ function sort_menu($items)
     return $items;
 }
 
+//****************************************************************************
+//Gets list items from a .txt file
 function get_items($file, $array)
 {
-    if(is_readable($file))
-    {
-        $handle = fopen($file, 'r');
-        $contents = trim(fread($handle, filesize($file)));
-        $list = explode("\n", $contents);
-            foreach ($list as $value)
-            {
-                array_push($array, $value);
-            }
-    }
-     fclose($handle);
-     return($array);
+    $handle = fopen($file, 'r');
+    $contents = trim(fread($handle, filesize($file)));
+    $list = explode("\n", $contents);
+        foreach ($list as $value)
+        {
+            array_push($array, $value);
+        }
+    fclose($handle);
+    return $array;
 }
 
+//****************************************************************************
+//Saves todo list items to a new file
+function newfile($new_file, $array)
+{
+    $handle = fopen($new_file, 'w');
+        foreach ($array as $value)
+        {
+        fwrite($handle, $value . PHP_EOL);
+        }
+    fclose($handle);
+}
+
+//****************************************************************************
 // The loop!
 do
 {
     // Echo the list produced by the function
     echo list_items($items);
     // Show the menu options
-    echo '(N)ew item, (S)ort (R)emove item, (F)ile Menu, (Q)uit : ';
+    echo '(N)ew item, (S)ort (R)emove item, (O)pen File, s(A)ve, (Q)uit : ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
     $input = get_input(TRUE);
 
+//****************************************************************************
+//Hidden commands for 'power users' removes list items from either the beginning or end
     if ($input == 'F')
     {
         array_shift($items);
@@ -118,12 +129,51 @@ do
         // Remove from array
         unset($items[$key - 1]);
     }
-    elseif ($input == 'F')
+
+//****************************************************************************
+//Allows the user to open a file and import the information into todo list
+    elseif ($input == 'O')
     {
         echo 'Open File: ';
         $file = get_input();
+        if(is_readable($file)){
         $items = get_items($file, $items);
+        }
+        else
+        {
+            echo "This is not a valid file name.\n";
+        }
     }
+
+//****************************************************************************
+//Allows user to save a todo list
+    elseif ($input == 'A')
+    {
+        echo 'Save file to: ';
+        $new_file = get_input();
+        if(file_exists($new_file))
+        {
+            echo "This file already exists. Saving will overwrite it. Are you sure you would like to save?\n";
+            echo "(Y)es or (N)o: ";
+            $input = get_input(TRUE);
+            if($input == 'Y')
+            {
+                newfile($new_file, $items);
+                echo "Save successful!\n";
+            }
+            else
+            {
+                echo "File not saved. Sorry.\n";
+            }
+        }
+        else
+        {
+            newfile($new_file, $items);
+            echo "Save successful!\n";
+        }
+    }
+
+//****************************************************************************
 // Exit when input is (Q)uit
 }
 while ($input != 'Q');
